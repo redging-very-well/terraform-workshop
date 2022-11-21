f0de935440ce2cc6cb750f85f7db797a40889758
Chrusty <>
2022-11-22 12:46:25 +1300

    formatting outputs

--- workshop/1.1/modules/storage/aws/outputs.tf
+++ workshop/1.1/modules/storage/aws/outputs.tf
@@ -1,3 +1,3 @@
 output "id" {
-    value = aws_s3_bucket.this.arn
+  value = aws_s3_bucket.this.arn
 }
--- workshop/1.1/modules/storage/azure/outputs.tf
+++ workshop/1.1/modules/storage/azure/outputs.tf
@@ -1,3 +1,3 @@
 output "id" {
-    value = azurerm_storage_container.this.id
+  value = azurerm_storage_container.this.id
 }
